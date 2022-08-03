b7c324b75cbc73fa087f1838395d6d5de3c54ed0
Marcus Krahl <Marcus.Krahl@t-systems.com>
2022-08-03 08:27:25 +0200

    initial commit

---
+++ Backend/db_utilities.js
@@ -0,0 +1,73 @@
+function select(db, table) {
+    return new Promise((resolve, reject) => {
+        const queries = [];
+        db.each(`SELECT rowid, * FROM ${table}`, (err, row) => {
+            if (err) {
+                reject(err); // optional: you might choose to swallow errors.
+            } else {
+                queries.push(row); // accumulate the data
+            }
+        }, (err, n) => {
+            if (err) {
+                reject(err); // optional: again, you might choose to swallow this error.
+            } else {
+                resolve(queries); // resolve the promise
+            }
+        });
+    });
+}
+
+const mapToValue = (v) => {
+    if (typeof v === 'string') {
+        return `'${v}'`
+    }
+    return v;
+}
+
+function insert(db, table, entity) {
+    return new Promise((resolve, reject) => {
+        const columns = Object.keys(entity);
+        const values = Object.values(entity).map(mapToValue); 
+        db.exec(`INSERT INTO ${table} (${columns.join(',')}) VALUES (${values.join(',')}) `, (err, done) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(done);
+            }
+        });
+    });
+}
+
+function update(db, table, entity) {
+    return new Promise((resolve, reject) => {
+        const updateStatements = Object.entries(entity).filter(([key]) => key.toLowerCase() != 'rowid').map(([key,value]) => `${key} = ${mapToValue(value)} `).join(',')
+        const columns = Object.keys(entity);
+        const values = Object.values(entity).map(v => typeof v === 'string' ? `'${v}'` : v); 
+        db.exec(`UPDATE ${table} SET ${updateStatements} WHERE rowid = ${entity.rowid} `, (err, done) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(done);
+            }
+        });
+    });
+}
+
+function deleteRow(db, table, rowId) {
+    return new Promise((resolve, reject) => {
+        db.exec(`DELETE FROM ${table} WHERE rowid = ${rowId}`, (err, done) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(done);
+            }
+        });
+    });
+}
+
+module.exports = {
+	insert,
+	update,
+	deleteRow,
+	select
+}
---
+++ Backend/index.js
@@ -0,0 +1,42 @@
+/*
+ * BACKEND 
+ */
+
+const express = require('express')
+const app = express()
+const port = 8080
+const sqlite3 = require('sqlite3').verbose();
+const { select, insert, update, deleteRow} = require('./db_utilities');
+const db = new sqlite3.Database('Datenbank/daten.sqlite');
+
+app.use(express.json())
+
+
+/* Hier beginnen die Backend Methoden - erreichbar unter http://localhost:8080/api */
+
+app.get('/api/', (req, res) => {
+  res.send(`<h1>${new Date}</h1>`);
+});
+
+app.post('/api/login', async (req, res) => {
+	let userdaten = await select(db, 'userdaten');
+	let password = req.body.password
+	let username = req.body.username
+	let user = userdaten.find(x => x.username == username)
+	if (user != null && password == user.password){
+		res.send("passt")
+	}
+	else {
+		res.status (401)
+		res.send ("passt nicht")
+	}
+});
+
+/* Hier enden die Backend Methoden */
+
+app.use(express.static('Frontend'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// db.close();
