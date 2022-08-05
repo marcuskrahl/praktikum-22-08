020d05f87ebe6f214631537927767d5f8b12ffb4
Jonathan Baade <jabd@t-systems-mms.com>
2022-08-05 10:17:21 +0200

    comment backend out

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@
  * BACKEND 
  */
 
-function keks_öffnen 
+// function keks_öffnen 
 
 
 
@@ -25,57 +25,57 @@ function keks_öffnen
 
 
 
-const express = require('express')
-const app = express()
-const port = 8080
-const sqlite3 = require('sqlite3').verbose();
-const { select, insert, update, deleteRow} = require('./db_utilities');
-const db = new sqlite3.Database('Datenbank/daten.sqlite');
+// const express = require('express')
+// const app = express()
+// const port = 8080
+// const sqlite3 = require('sqlite3').verbose();
+// const { select, insert, update, deleteRow} = require('./db_utilities');
+// const db = new sqlite3.Database('Datenbank/daten.sqlite');
 
-app.use(express.json())
-
-
-/* Hier beginnen die Backend Methoden - erreichbar unter http://localhost:8080/api */
-
-app.get('/api/', (req, res) => {
-  res.send(`<h1>${new Date}</h1>`);
-});
-
-app.post('/api/login', async (req, res) => {
-	let userdaten = await select(db, 'userdaten');
-	let password = req.body.password
-	let username = req.body.username
-	let user = userdaten.find(x => x.username == username)
-	if (user != null && password == user.password){
-		res.send("passt")
-	}
-	else {
-		res.status (401)
-		res.send ("passt nicht")
-	}
-});
-
-/* Hier enden die Backend Methoden */
-
-app.use(express.static('Frontend'));
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-// db.close();
+// app.use(express.json())
 
 
+// /* Hier beginnen die Backend Methoden - erreichbar unter http://localhost:8080/api */
+
+// app.get('/api/', (req, res) => {
+//   res.send(`<h1>${new Date}</h1>`);
+// });
+
+// app.post('/api/login', async (req, res) => {
+// 	let userdaten = await select(db, 'userdaten');
+// 	let password = req.body.password
+// 	let username = req.body.username
+// 	let user = userdaten.find(x => x.username == username)
+// 	if (user != null && password == user.password){
+// 		res.send("passt")
+// 	}
+// 	else {
+// 		res.status (401)
+// 		res.send ("passt nicht")
+// 	}
+// });
+
+// /* Hier enden die Backend Methoden */
+
+// app.use(express.static('Frontend'));
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+// // db.close();
 
 
-<script>
 
-async function login() {
-	let body = JSON.stringify({username: 'test123', password: 'test123'});
-	let response = await fetch('/api/login', { method: 'POST', body: body , headers: {'Content-Type':'application/json'}});
-	if (response.status === 401) {
-		//fehlgeschlagen
-	} else {
-		//erfolgreich
-	}
-}
-</script>
+
+// <script>
+
+// async function login() {
+// 	let body = JSON.stringify({username: 'test123', password: 'test123'});
+// 	let response = await fetch('/api/login', { method: 'POST', body: body , headers: {'Content-Type':'application/json'}});
+// 	if (response.status === 401) {
+// 		//fehlgeschlagen
+// 	} else {
+// 		//erfolgreich
+// 	}
+// }
+// </script>
