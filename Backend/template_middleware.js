de08a196f903f218ff1ede1a4c6763fccaa0a498
Marcus Krahl <Marcus.Krahl@t-systems.com>
2022-08-11 14:33:36 +0200

    template middleware aktualisiert

--- Backend/template_middleware.js
+++ Backend/template_middleware.js
@@ -4,12 +4,16 @@ const fs = require('fs');
 
 
 function templateMiddleware(req, res, next) {
-  if (!req.originalUrl.endsWith('.html')) {
+  let file = req.originalUrl;
+  if (req.originalUrl === '/') {
+    file = `/index.html`;
+  }
+  if (!file.endsWith('.html')) {
     next();
     return;
   }
   const template = fs.readFileSync('Frontend/template.html', { encoding: 'utf-8'});
-  const location = `Frontend${req.originalUrl}`;
+  const location = `Frontend${file}`;
   fs.readFile(location, {encoding: 'utf-8'}, (error, data) => {
     if (error) {
       res.sendStatus(404);
